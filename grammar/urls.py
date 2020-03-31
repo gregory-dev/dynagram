5bd78faaa74be70074f1c8db9450dcc03143a394
Nicholas Piano <nicholas.d.piano@gmail.com>
2020-03-31 20:19:47 +0200

    Add test method

---
+++ grammar/urls.py
@@ -0,0 +1,43 @@
+
+from .methods import methods
+
+from django.http import JsonResponse
+from django.urls import path, include
+
+def info(method):
+  def view(request):
+    return JsonResponse(method.requirements)
+
+  return view
+
+def generate_paths(method):
+  name = method.__name__
+
+  return path(
+    '{}/'.format(name),
+    include([
+      path('info/', info(method)),
+      path('run/', method),
+    ]),
+  )
+
+def methods_info(request):
+  info = {
+    method.__name__: method.requirements
+    for method
+    in methods
+  }
+
+  return JsonResponse(info)
+
+methods_urlpatterns = [
+  generate_paths(method)
+  for method
+  in methods
+]
+
+info_urlpatterns = [
+  path('info/', methods_info),
+]
+
+urlpatterns = methods_urlpatterns + info_urlpatterns
